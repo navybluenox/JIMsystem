819de3eacde5df09291612da21c2aca649340e01
navybluenox <navyblueliktt08@gmail.com>
2016-07-15 14:09:10 +0900

    ""

--- gas/JIMSystem/commit/main.js
+++ gas/JIMSystem/commit/main.js
@@ -4,11 +4,14 @@ function myFunction() {
 
 
 var this_checkPass = Script.checkPass.bind(this,"jimjim");
-var this_include = Script.include.bind(this);
 
 function doGet(request) {
   return HtmlService.createTemplateFromFile('html_login')
       .evaluate()
       .setSandboxMode(HtmlService.SandboxMode.IFRAME)
       .setTitle("JIMシステム_forJIMs");
+}
+
+function include(filename) {
+    return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
