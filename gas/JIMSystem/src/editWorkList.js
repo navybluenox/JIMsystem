c8e74c23c8fb93b9b882ac41ae2793f8fe942863
navybluenox <navyblueliktt08@gmail.com>
2016-10-05 16:10:55 +0900

    ""

--- gas/JIMSystem/src/editWorkList.js
+++ gas/JIMSystem/src/editWorkList.js
@@ -1,7 +1,19 @@
 $(function(){
+    var workListEditing;
     _val.pageFun.editWorkList = {
         onload:function(){
+            _val.server.loadData("user");
+            _val.server.loadData("workList");
+        },updateWorkList:function(kind,_id){
+            var workList;
+            if(kind === "add"){
 
+            }else if(kind === "change"){
+
+            }else if(kind === "remove"){
+
+            }
+            //_val.server.sendUpdateQueue();
         },searchUserIdByAzusa:function(el){
             el = $(el);
             console.log(el);
