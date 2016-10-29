fcb228e05555d87f5ff1c55285f449f034c065b5
navybluenox <navyblueliktt08@gmail.com>
2016-10-29 15:26:59 +0900

    ""

--- gas/uploadScripts/gs_babel_es5/base.js
+++ gas/uploadScripts/gs_babel_es5/base.js
@@ -437,7 +437,8 @@ function checkSimplePass(pass, keyName, systemName, pageName) {
         result = key === pass;
     } else {
         if (key === pass) {
-            result = ThisScript[systemName].runAsAtThisScript("getPage", [pageName]);
+            //result = ThisScript[systemName].runAsAtThisScript("getPage",[pageName]);
+            result = ThisScript[systemName].getPage(pageName);
         } else {
             result = null;
         }
