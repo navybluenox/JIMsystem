b27561b20234db43c241b3d0fd6df579e1890fc8
navybluenox <navyblueliktt08@gmail.com>
2017-04-25 02:15:38 +0900

    "revise Incharge Class a little (line 1289)"

--- script/database.js
+++ script/database.js
@@ -1286,6 +1286,7 @@ class WorkList extends Datapiece{
 
 class Incharge extends Datapiece{
     constructor(datapieceObj,option){
+        if(option === undefined)  option = {};
         super(datapieceObj, option.dataName === undefined ? "incharge" : option.dataName, option);
     }
 }
